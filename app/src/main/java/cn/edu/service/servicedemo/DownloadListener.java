200f891d6fcbb438adb38680f86762864eab2378
yanpan <Lggbxf@163.com>
2018-12-16 13:42:47 +0800

    create Service Demo

---
+++ app/src/main/java/cn/edu/service/servicedemo/DownloadListener.java
@@ -0,0 +1,9 @@
+package cn.edu.service.servicedemo;
+
+public interface DownloadListener {
+    void onProgress(int progress);
+    void onSuccess();
+    void onFailed();
+    void onPaused();
+    void onCanceled();
+}
